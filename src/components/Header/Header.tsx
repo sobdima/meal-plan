fd5e7190048e6182cfb4a13e1d077e3921640483
Dima Sob <sobolevskii.dmit@gmail.com>
2026-02-17 23:30:18 +0100

    feat: add header markup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,7 @@
+import './Header.css';
 import { Button } from '@/components/Button/Button';
+import { LogoIcon } from '../Icons/LogoIcon';
+import { AddButtonIcon } from '../Icons/AddButtonIcon';
 
 export function Header() {
   const handleGenerate = () => {
@@ -11,33 +14,24 @@ export function Header() {
 
   return (
     <header className="header">
-      <div className="header-logo">
-        <h1>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            /* className="h-6 w-6" */
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
-            ></path>
-          </svg>
+      <div className="logo-container">
+        <h1 className="logo-title">
+          <LogoIcon />
           MealPlan
         </h1>
       </div>
 
       <div className="header-buttons">
         <Button
+          text={<AddButtonIcon />}
+          onClick={handleAdd}
+          className="btn-add"
+        />
+        <Button
           text="Generate"
           onClick={handleGenerate}
           className="btn-generate"
         />
-        <Button text="+" onClick={handleAdd} className="btn-add" />
       </div>
     </header>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/reset.css';
 import './styles/variables.css';
+import './main.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
